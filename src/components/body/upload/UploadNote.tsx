b77ec8850a2d40fcdf5bd4083ae13cbb740d012e
mac <dean@decompany.io>
2020-06-18 15:43:10 +0900

    - edit word cancel to close on complete modal - apply disabled option on upload page when complete upload

--- src/components/body/upload/UploadNote.tsx
+++ src/components/body/upload/UploadNote.tsx
@@ -1,18 +1,21 @@
 import React, { ReactElement } from 'react'
 import { psString } from '../../../util/localization'
+import { commonNative } from '../../../util/commonNative'
+import { APP_CONFIG } from '../../../util/app.config'
 
 export default function(): ReactElement {
+  const onOpenWindow = () => {
+    commonNative.setWindowOpenUrl(APP_CONFIG.domain().ps)
+    document.getElementById('openWindow')!.click()
+  }
+
   return (
     <div className="u_note">
       <span>Note:</span>
       {psString('upload-explain-1')}
-      <a href={'https://polarishare.com/au'} target="_blank">
-        {psString('upload-explain-2')}
-      </a>
+      <div className="u_link" onClick={() => onOpenWindow()}>{psString('upload-explain-2')}</div>
       {psString('upload-explain-3')}
-      <a href={'https://polarishare.com/au'} target="_blank">
-        {psString('upload-explain-4')}
-      </a>
+      <div className="u_link" onClick={() => onOpenWindow()}>{psString('upload-explain-4')}</div>
       <div className="u_noteSub">({psString('upload-explain-sub')})</div>
     </div>
   )
